8aaa7f3ab486a61377bd8c60297965499e54d33d
Juan Carlos Gómez Sierra <jcgs2001@msn.com>
2021-08-01 09:07:27 -0500

    Creacion Solucion y proyecto API

---
+++ Persona/Persona.API/FakeDb/PersonsFakeDb.cs
@@ -0,0 +1,87 @@
+﻿using Persona.API.Models;
+using System.Collections.Generic;
+
+namespace Persona.API.FakeDB
+{
+    public class PersonsFakeDb
+    {
+        private List<Person> _fakePerson = new List<Person>
+        {
+            new Person
+            {
+                Id = 1,
+                Name = "Juan",
+                Age = 25,
+                Email = "jcgs2001@msn.com",
+                Adress = "Cll 184 11- 75",
+                Phone = "2345678",
+                Mobile = "3016417088",
+                State = true,
+                BirthDate = System.DateTime.Now
+            },
+            new Person
+            {
+                Id = 2,
+                Name = "Carolina",
+                Age = 25,
+                Email = "caro@msn.com",
+                Adress = "Cll 184 11- 75",
+                Phone = "2345678",
+                Mobile = "3016417088",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 110141
+            },
+            new Person
+            {
+                Id = 3,
+               Name = "Maria",
+                Age = 25,
+                Email = "Maria@msn.com",
+                Adress = "Cll 184 11- 75",
+                Phone = "2345678",
+                Mobile = "3016417088",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 110141
+            },
+            new Person
+            {
+                Id = 4,
+                Name = "Lucas",
+                Age = 25,
+                Email = "lucas@msn.com",
+                Adress = "Cll 184 11- 75",
+                Phone = "2345678",
+                Mobile = "3016417088",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 110141
+            },
+            new Person
+            {
+                Id = 5,
+                Name = "Eduardo",
+                Age = 25,
+                Email = "Edward@msn.com",
+                Adress = "Cll 184 11- 75",
+                Phone = "2345678",
+                Mobile = "3016417088",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 110141v
+            },
+        };
+        public List<Person> FakePersons
+        {
+            get
+            {
+                return _fakePerson;
+            }
+            set
+            {
+                _fakePerson = FakePersons;
+            }
+        }
+    }
+}
---
+++ Persona/Persona.API/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Persona.API.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public bool State { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
