0eb18bf326126501317f2faaf2d8cf8be617c173
Juan Carlos Gómez Sierra <jcgs2001@msn.com>
2021-08-02 08:31:37 -0500

    More validations Test

--- Persona/Person.Test/PersonControllerTests.cs
+++ Persona/Person.Test/PersonControllerTests.cs
@@ -75,12 +75,89 @@ namespace Person.Tests
             //Arrange
             var person = new Person
             {
-                Name = "Name",
+                Name = "Luis",
                 Age = 0,
                 Email = "string@fff.com",
                 Adress = "string",
                 Phone = "2345678",
                 Mobile = "3052289420",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 213450
+
+            };
+
+            //Act
+            var result = _controller.Save(person);
+
+            //Assert
+            Assert.IsAssignableFrom<BadRequestObjectResult>(result.Result);
+        }
+
+        
+
+        [Fact]
+        public void Should_return_BadRequest_for_validation_of_model_failure_email_empty()
+        {
+            //Arrange
+            var person = new Person
+            {
+                Name = "Name",
+                Age = 12,
+                Email = "",
+                Adress = "string",
+                Phone = "2345678",
+                Mobile = "3052289420",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 213450
+
+            };
+
+            //Act
+            var result = _controller.Save(person);
+
+            //Assert
+            Assert.IsAssignableFrom<BadRequestObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void Should_return_BadRequest_for_validation_of_model_failure_Phone_with_letters()
+        {
+            //Arrange
+            var person = new Person
+            {
+                Name = "Daniel",
+                Age = 12,
+                Email = "daniel@hhh.com",
+                Adress = "string",
+                Phone = "23dj678",
+                Mobile = "3052289420",
+                State = true,
+                BirthDate = System.DateTime.Now,
+                ZipCode = 213450
+
+            };
+
+            //Act
+            var result = _controller.Save(person);
+
+            //Assert
+            Assert.IsAssignableFrom<BadRequestObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void Should_return_BadRequest_for_validation_of_model_failure_Mobile_length()
+        {
+            //Arrange
+            var person = new Person
+            {
+                Name = "Daniel",
+                Age = 12,
+                Email = "daniel@hhh.com",
+                Adress = "string",
+                Phone = "23dj678",
+                Mobile = "30522",
                 State = true,
                 BirthDate = System.DateTime.Now,
                 ZipCode = 213450
